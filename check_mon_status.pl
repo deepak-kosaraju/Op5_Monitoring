9e8b2207dfb41d9b8c86b6a6b25564f739ea947c
Deepak Kosaraju <deepak.kosaraju1@gmail.com>
2012-03-13 17:57:51 -0400

    Modified the Usage of check_mon_status.pl

--- check_mon_status.pl
+++ check_mon_status.pl
@@ -189,20 +189,31 @@ sub mail{
 }
 
 sub usage {
-	print "Copyright (c) 2011 Deepak Kosaraju\n\n";
-	print "Dependencies:-\n";
-	print "\tPerl module Mail::Mailer to be installed to send emails\n\n";
-	print "\tHelps to monitor the status of Op5 instances in both Distributed and Stanlone Environment, for now it is only helps to monitor the Distributed setup between poller and master, but not master with peer master. Health Status of pollers in Distributed Enviornment is monitored by this watchdog Heatlh Check wrapper via cron job. At any point in time if poller (or) master instance are INACTIVE this Selfheal wrapper will attempt to restart the monitoring instances that are INACTIVE, if it fails to selfhealt an email notificaiton is sentout to supplied email id's.\n\n";
-	print "\t #### NOTE: RECOVERY EMAIL's ARE ONLY SENT TO EMAIL THAT IS FIRST IN THE LIST THAT IS FEEDED WITH -e OPTION ### \n\n";
-	print "** Add following cron entry to a file in '/etc/cron.d/mon_status'.\n\n";
-	print "####\n";
-	print "*/5 * * * * root /opt/plugins/custom/check_mon_status -e < place email id's here>\n";
-	print "####\n\n";
-        print "Usage:\n";
-	print "\t./$PROGNAME [-e <email address | multiple addresses can be seprate by ,>]\n\n";
-        print "\t[ -e All emails who need to get notifications separated by , ]\n\n";
-	print "\t[ -t | --timeout <default is 180sec>]\n\n";
-	print "\t[ -v | --verbose ]\n\n";
-	print "\t[ -h | --help ]\n\n";
+	print <<EOF;
+Copyright (c) 2011 Deepak Kosaraju
 	
+	## Dependencies:- Perl module Mail::Mailer to be installed to send emails ##
+	
+	Helps to monitor the status of Op5 instances in both Distributed and Stanlone Environment, for now it is only helps to monitor the Distributed setup between poller and master, but not master with peer master. Health Status of pollers in Distributed Enviornment is monitored by this watchdog Heatlh Check wrapper via cron job. At any point in time if poller (or) master instance are INACTIVE this Selfheal wrapper will attempt to restart the monitoring instances that are INACTIVE, if it fails to selfheal an email notificaiton is sentout to supplied email id's
+	
+#### NOTE: RECOVERY EMAIL's ARE ONLY SENT TO EMAIL THAT IS FIRST IN THE LIST THAT IS FEEDED WITH -e OPTION ###
+	
+** Add following cron entry to a file in '/etc/cron.d/mon_status **
+	
+####
+*/5 * * * * root /opt/plugins/custom/check_mon_status -e < place email id's here>
+####
+  
+Usage: ./$PROGNAME [-e <email address | multiple addresses can be seprate by ,>]
+	
+	-e  < All emails who need to get notifications separated by , >
+	
+	-t | --timeout <default is 180sec>
+	
+	-v | --verbose
+	
+	-h | --help
+
+EOF
+	  exit(3);
 }
